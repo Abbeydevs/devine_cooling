d7d3750f86862ba422fa57eef14f518b354651f5
abbeydevs <abbey.developer@gmail.com>
2022-04-26 14:12:06 +0100

    Updates

--- js/index.js
+++ js/index.js
@@ -6,3 +6,15 @@ window.addEventListener('scroll', function () {
     header.classList.toggle('scrolling-active', windowPosition);
 })
 
+
+
+const openFirstSlideModal = document.getElementById("first-slide-modal");
+const closeFirstSlideModal = document.getElementById("first-slide-modal");
+
+function openFirstModal() {
+    openFirstSlideModal.style.display = "block";
+}
+
+function closeFirstModal() {
+    closeFirstSlideModal.style.display = "none";
+}
